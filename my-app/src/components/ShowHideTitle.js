6f003b36ae68a6fa904421e42d6eff97c70fca62
patriceandrea <andrea_pavico98@yahoo.ca>
2023-02-20 12:21:12 -0500

    complete challenge #1

---
+++ my-app/src/components/ShowHideTitle.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+const ShowHideTitle = () => {
+  //if the you click on the button, the show state turn into true
+  //once the show state is true, 
+
+  const [show, setShow] = useState(false);
+
+  const handleClick = () => {
+    return setShow(!show);
+  }
+
+  return (
+    <>
+      <h1>Challenge #1</h1>
+      <p>Implement a button to show/hide the text content:</p>
+      <button onClick={handleClick}>Show/Hide</button>
+      {show && <a>Welcome to React Challenges</a>}
+
+    </>
+  )
+}
+
+export default ShowHideTitle; 
